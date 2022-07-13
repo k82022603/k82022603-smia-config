1c550a811ae68d5ef165344402313e14ed95ce72
k82022603 <k82022603@gmail.com>
2022-07-13 14:00:41 +0900

    20220713-01 Init

---
+++ k82022603-smia-config-copy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -x #echo on
+cp -Rv ~/SpringMicroservicesInAction/k82022603-smia/chapter5/configserver/src/main/resources/config/* ~/SpringMicroservicesInAction/k82022603-smia-config/.
+
